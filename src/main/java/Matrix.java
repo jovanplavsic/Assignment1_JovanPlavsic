d3d0a8c7a5c26633bbe5d92addc4d7ca19eac626
jovanplavsic <jovanplavsic05@gmail.com>
2025-04-26 11:54:32 -0500

    Add Matrix.java, implement getters and setter

---
+++ src/main/java/Matrix.java
@@ -0,0 +1,10 @@
+public class Matrix {
+    private int [][] data;
+    public void setData(int[][] data){
+        this.data = data;
+    }
+
+    public int[][] getData(){
+        return data;
+    }
+}
