be8da088344cddf98641cbfcbe6e2f581c3fb748
jovanplavsic <jovanplavsic05@gmail.com>
2025-04-26 12:50:52 -0500

    Fix variable name bug

--- src/test/java/MatrixTest.java
+++ src/test/java/MatrixTest.java
@@ -1,20 +1,21 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class MatrixTest {
 
     @Test
     public void testValidDimensions() {
-        Matrix0 matrix = new Matrix0(3, 4);
+        Matrix matrix = new Matrix(3, 4);
         assertEquals(3, matrix.getRowCount());
         assertEquals(4, matrix.getColCount());
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testZeroConstructors() {
-        new Matrix0(0, 0);
-        new Matrix0(0, 1);
-        new Matrix0(1, 0);
-        new Matrix0(1, -1);
-        new Matrix0(-1, 1);
+        new Matrix(0, 0);
+        new Matrix(0, 1);
+        new Matrix(1, 0);
+        new Matrix(1, -1);
+        new Matrix(-1, 1);
     }
 }
