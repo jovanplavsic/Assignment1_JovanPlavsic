95fe8e2aa75e6a13d19b10b1c161a4f7ee13c510
jovanplavsic <jovanplavsic05@gmail.com>
2025-04-26 12:00:06 -0500

    Implement constructor and overload constructor

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,5 +1,20 @@
 public class Matrix {
     private int [][] data;
+
+    public Matrix(int rows, int cols){
+        if (rows < 1 || cols < 1){
+            throw new IllegalArgumentException("Requires at least 1 row and 1 column");
+        }
+        data = new int[rows][cols];
+    }
+
+    public Matrix(int [][] data){
+        if (data == null || data.length < 1 || data[0].length < 1) {
+            throw new IllegalArgumentException("Cannot be null and requires at least 1 row and 1 column");
+        }
+        this.data = data;
+    }
+
     public void setData(int[][] data){
         this.data = data;
     }
