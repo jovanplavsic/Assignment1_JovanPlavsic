a72318e209ab0e2bea7c7d04887c7a716581bca3
jovanplavsic <jovanplavsic05@gmail.com>
2025-04-26 13:49:01 -0500

    Clear methods in order to rewrite correctly

--- src/main/java/MatrixOutlinePrinter.java
+++ src/main/java/MatrixOutlinePrinter.java
@@ -1,37 +1,18 @@
-import java.util.Scanner;
-
 public class MatrixOutlinePrinter implements MatrixPrinter {
 
     @Override
     public String print(Matrix matrix) {
-        StringBuilder output = new StringBuilder();
-        StringBuilder cols = new StringBuilder();
-        StringBuilder row = new StringBuilder();
-
-        int r = matrix.getRowCount();
-        int c = matrix.getColCount();
-
-        cols.append("+");
-        row.append("|");
-        for (int j = 0; j < c; j++) {
-            cols.append("-");
-            row.append(" ");
-        }
-        cols.append("+");
-        row.append("|");
-
-        output.append(cols).append("\n");
-        for (int j = 0; j < r; j++) {
-            output.append(row).append("\n");
-        }
-        output.append(cols);
+        int rows = matrix.getRowCount();
+        int cols = matrix.getColCount();
+        StringBuilder out = new StringBuilder();
 
 
-        return output.toString();
     }
 
-    protected String printRow(Matrix matrix, int rowIndex) {
-        return "";
-    }
 
+    protected String print(Matrix matrix, int rowIndex) {
+        StringBuilder row = new StringBuilder("|");
+
+        return row.toString();
+    }
 }
