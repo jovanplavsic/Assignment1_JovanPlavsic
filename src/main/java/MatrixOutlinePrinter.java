87bfcb5d5272712e387bb62090fb324e9df65ac6
jovanplavsic <jovanplavsic05@gmail.com>
2025-04-26 13:52:09 -0500

    Fix print to correctly print the rest of matrix using the protected function.

--- src/main/java/MatrixOutlinePrinter.java
+++ src/main/java/MatrixOutlinePrinter.java
@@ -6,6 +6,19 @@ public class MatrixOutlinePrinter implements MatrixPrinter {
         int cols = matrix.getColCount();
         StringBuilder out = new StringBuilder();
 
+        out.append("+");
+        for (int j = 0; j < cols; j++) out.append("-");
+        out.append("+\n");
+
+        for (int i = 0; i < rows; i++) {
+            out.append(print(matrix, i)).append("\n");
+        }
+
+        out.append("+");
+        for (int j = 0; j < cols; j++) out.append("-");
+        out.append("+");
+
+        return out.toString();
 
     }
 
